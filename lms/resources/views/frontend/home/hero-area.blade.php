ce4c1151310aa8f25a32d520e35b94ce004eb39b
aryo <serchostz@gmail.com>
2024-12-17 00:16:46 +0700

    Upload Project

---
+++ lms/resources/views/frontend/home/hero-area.blade.php
@@ -0,0 +1,59 @@
+<section class="hero-area">
+    <div class="hero-slider owl-action-styled">
+        <div class="hero-slider-item hero-bg-1">
+            
+            <div class="container">
+                <div class="hero-content">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">We Help You Learn <br> What You Love</h2>
+                        <p class="section__desc text-white pb-4">Unlock your potential with our expert-led courses designed to inspire and educate. 
+                            <br>Join thousands of learners and gain the skills you need to succeed in today's competitive landscape. 
+                            <br>Discover a passion for learning and take the first step towards your dreams with our comprehensive e-courses.</p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1">
+                        <a href="{{ route('register') }}" class="btn theme-btn mr-4 mb-4">Join with Us <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- end container -->
+        </div><!-- end hero-slider-item -->
+        <div class="hero-slider-item hero-bg-2">
+            <div class="container">
+                <div class="hero-content text-center">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Join BTSolution & Get <br> Your Free Courses!</h2>
+                        <p class="section__desc text-white pb-4">Kickstart your learning journey with BTSolution! 
+                            <br>Sign up now and access a variety of free courses designed to help you grow and succeed. 
+                            <br>Whether you're looking to enhance your skills or explore new interests, our courses provide the perfect opportunity to learn at your own pace.</p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-center">
+                        <a href="{{ route('register') }}" class="btn theme-btn mr-4 mb-4">Get Started <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div><!-- end hero-slider-item -->
+        <div class="hero-slider-item hero-bg-3">
+            <div class="container">
+                <div class="hero-content text-right">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Learn Anything, <br> Anytime, Anywhere</h2>
+                        <p class="section__desc text-white pb-4">Discover the freedom to learn on your own terms with our flexible e-courses. 
+                            <br>Access a wide range of subjects and gain new skills from the comfort of your home or on the go. 
+                            <br>With BTSolution, education is always within reach, empowering you to achieve your goals whenever and wherever you choose.</p>                            
+                    </div>
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-end">
+                        <a href="#" class="btn-text video-play-btn mr-4 mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            <i class="la la-play icon-btn mr-2"></i>Watch Preview
+                        </a>
+                        <a href="{{ route('register') }}" class="btn theme-btn mb-4"><i class="la la-arrow-left icon mr-1"></i>Get Enrolled </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div><!-- end hero-slider-item -->
+    </div><!-- end hero-slide -->
+</section><!-- end hero-area -->
